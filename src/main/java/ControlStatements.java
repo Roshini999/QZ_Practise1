929df2835807a0aeeebffa39c7599f676015220c
Roshini <roshini.bunga9@gmail.com>
2024-03-19 15:41:06 +0530

    Added Java Control Statements.

---
+++ src/main/java/ControlStatements.java
@@ -0,0 +1,13 @@
+package com.qualizeal.javaapp.company.csif;
+
+public class ControlStatements {
+
+	public void cs() {
+
+		int i = 100;
+		if (i < 15)
+		  System.out.println("10 is less than 15");
+		System.out.println("Outside if-block");
+		
+	}
+}
---
+++ src/main/java/Program.java
@@ -0,0 +1,18 @@
+package com.qualizeal.javaapp.company;
+
+
+import com.qualizeal.javaapp.company.csif.ControlStatements;
+
+public class Program {
+
+	public static void main(String [] args) {
+
+			
+		ControlStatements controlstate = new ControlStatements();
+		controlstate.cs();	
+
+
+	}
+	
+
+}
